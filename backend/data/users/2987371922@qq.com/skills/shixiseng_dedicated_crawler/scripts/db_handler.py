b0ea0d9f7de8a4f85e325c0189fa25648603cba5
ggd <2987371922@qq.com>
2026-05-22 20:31:18 +0800

    chore: 取消跟踪运行时用户数据

--- backend/data/users/2987371922@qq.com/skills/shixiseng_dedicated_crawler/scripts/db_handler.py
+++
@@ -1,8 +0,0 @@
-from file_store import FileStore
-
-
-class DBHandler(FileStore):
-    """Backward-compatible alias for the old DB-oriented name."""
-
-    def __init__(self, db_path=None):
-        super().__init__(storage_dir=db_path)
